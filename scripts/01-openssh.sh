9d36e93c273850829283ea55bdfc5c7d2aad6ef7
Paulo Cesar da Silva <pc.silva39@gmail.com>
2023-02-21 14:47:19 -0300

    atualizaçao

--- scripts/01-openssh.sh
+++ scripts/01-openssh.sh
@@ -8,7 +8,7 @@
 # Instagram: https://www.instagram.com/procedimentoem/?hl=pt-br
 # Github: https://github.com/vaamonde
 # Data de criação: 10/10/2021
-# Data de atualização: 24/03/2022
+# Data de atualização: 21/02/2023
 # Versão: 0.27
 # Testado e homologado para a versão do Ubuntu Server 20.04.x LTS x64
 # Testado e homologado para a versão do OpenSSH Server v8.2.x
